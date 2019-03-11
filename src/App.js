edec807a6e1be83df8f4b7a8e17eec2cb1f00297
OscarB <oballesteros@abstract.cl>
2019-03-10 21:57:19 -0300

    Add Error, Form and Header components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './componentes/Header';
+import Formulario from './componentes/Formulario';
+import Error from './componentes/Error';
+
 
 class App extends Component {
+
+  state = {
+    error: '', 
+    consulta : {}
+  }
+
+  componentDidUpdate() {
+    this.consultarApi();
+  }
+
+  componentDidMount() {
+    this.setState({
+      error: false 
+    })
+  }
+
+  consultarApi = () => {
+    const { ciudad, pais } = this.state.consulta;
+    if(!ciudad || !pais) return null;
+
+    const appid = 'e3646bbc406abe0aa693067648112d7d';
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${ ciudad },${ pais }&appid=${appid}`;
+    console.log(url);
+    // query con fetch api
+   
+    // leer la url y agregar el Api key
+
+    // Consultar con fetch 
+ 
+  }
+
+  datosConsulta = respuesta => {
+    if(respuesta.ciudad === '' || respuesta.pais === ''){
+      this.setState({
+        error: true 
+      });
+    } else {
+      this.setState({
+        consulta: respuesta
+      });
+    }
+
+  }
+
   render() {
+
+    const error = this.state.error;
+
+    let respuesta; 
+
+    if(error) {
+      respuesta =  <Error mensaje="Ambos campos son obligatorios" />
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        
+        <Header 
+          titulo = 'Clima React' 
+        />
+
+        <Formulario
+          datosConsulta = { this.datosConsulta }
+        />
+
+        { respuesta }
       </div>
     );
   }
